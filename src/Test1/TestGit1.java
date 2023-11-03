5244c77e5499d2f524b51d50f74ea8489e886447
tozleblebici <talha43gs@gmail.com>
2023-11-03 18:38:06 +0000

    TestGit1 Uploaded

---
+++ src/Test1/TestGit1.java
@@ -0,0 +1,12 @@
+package Test1;
+
+public class TestGit1 {
+
+    public static void main(String[] args) {
+        System.out.println("GIT!!!");
+        System.out.println("Test1");
+        System.out.println("Test2");
+        System.out.println("Test3");
+        System.out.println("Test4");
+    }
+}
